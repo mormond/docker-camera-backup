0df54878c0646d52a7f24095b7f6d705b203ae90
Mike Ormond <mormond@microsoft.com>
2018-12-17 10:17:03 +0000

    Fix for not all Find options implemented

--- python/tidyup.py
+++ python/tidyup.py
@@ -2,7 +2,7 @@ import os
 import constants as c
 
 # Remove all empty directories from the FTP upload path
-os.system ('find ' + c.cameraPath + ' -empty -type d -delete')
+os.system ('find ' + c.cameraPath + ' -depth -type d -exec rmdir --ignore-fail-on-non-empty {} +')
 
 # Remove all archive files past a threshold date
-os.system ('find ' + c.archivePath + ' -mtime +' + str(c.archive_deletionThresholdDays) + ' -delete')
+os.system ('find ' + c.archivePath + ' -mtime +' + str(c.archive_deletionThresholdDays) + ' -type d -exec rm -r {} +')
